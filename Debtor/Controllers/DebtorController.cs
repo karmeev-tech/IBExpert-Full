f6dcc27c47f62882ed0502f93dd68b149ebe00c4
Kirill Karmeev <70718637+karmeev-tech@users.noreply.github.com>
2023-05-02 22:41:31 +0300

    base

---
+++ Debtor/Controllers/DebtorController.cs
@@ -0,0 +1,42 @@
+﻿using Debtor.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Debtor.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DebtorController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<DebtorItem>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(new DebtorItem()
+            {
+                Name = "Владим Владимирович Путин",
+                Status = "В работе"
+            });
+        }
+
+        // POST api/<DebtorController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<DebtorController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<DebtorController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ Debtor/Models/DebtorItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Debtor.API.Models
+{
+    public class DebtorItem
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+    }
+}
